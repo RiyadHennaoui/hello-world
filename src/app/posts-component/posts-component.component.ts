7327dc41a00bd40864364806bdeba1f5d32b1a9d
Riyad Hennaoui <riyad.hennaoui@gmail.com>
2022-04-07 11:25:07 +0200

    update method

--- src/app/posts-component/posts-component.component.ts
+++ src/app/posts-component/posts-component.component.ts
@@ -7,14 +7,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./posts-component.component.css']
 })
 export class PostsComponentComponent  {
-  posts!: any;
+  posts!: any[];
+  private url = 'https://jsonplaceholder.typicode.com/posts'; 
+  result: any;
+  titre: string ='';
 
-  constructor(http: HttpClient){
-    http.get('https://jsonplaceholder.typicode.com/posts')
+  // constructor(private http: HttpClient){
+  //   http.get(this.url)
+  //   .subscribe(response => {
+  //     console.log(response);
+  //     this.posts = response as [];
+  //     this.sortTabByIdDesc(this.posts);
+  //   });
+  // }
+
+  constructor(private http: HttpClient){
+    fetch(this.url)
+    .then(response => response.json())
+    .then(json => this.posts = json)
+    .then(() => this.sortTabByIdDesc(this.posts));
+    };
+    
+  
+
+  createPost(input: HTMLInputElement){
+      let post: any = { title: input.value};
+    this.http.post(this.url,JSON.stringify(post))
+    .subscribe(response => {
+      post.id = (response as any).id;
+      console.log(response, post.id, post);
+      this.posts.push(post);
+      this.titre = '';
+      this.sortTabByIdDesc(this.posts);
+    });
+  }
+
+  private sortTabByIdDesc(tab:any[]) {
+    tab.sort((a,b) => b.id - a.id);
+  }
+
+  updatePost(post){
+
+    this.http.patch(this.url + '/'+ post.id, JSON.stringify({ isRead: true}))
     .subscribe(response => {
       console.log(response);
-      this.posts = response;
-    });
+    })
+    // this.http.put(this.url, JSON.stringify(post));
+
   }
 
 }
